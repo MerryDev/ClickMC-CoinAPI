23a8c301ab785d0b5834bd3e7683ed086e1b3dd1
waddlespam <max@mlehrke.de>
2022-04-24 20:16:28 +0200

    Fixed bug

--- src/main/java/de/digitaldevs/playerapi/PlayerAPI.java
+++ src/main/java/de/digitaldevs/playerapi/PlayerAPI.java
@@ -1,6 +1,6 @@
 package de.digitaldevs.playerapi;
 
-import de.digitaldevs.database.mysql.MySQLConnector;
+import de.digitaldevs.database.mysql.MySQLHandler;
 import de.digitaldevs.playerapi.utils.MySQLConfig;
 import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -8,18 +8,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class PlayerAPI extends JavaPlugin {
 
     @Getter
-    public MySQLConnector mySQLConnector;
-
+    private MySQLHandler mySQLHandler;
     @Override
     public void onEnable() {
-        MySQLConfig config = new MySQLConfig();
+        final MySQLConfig config = new MySQLConfig();
         config.initDefault();
-        mySQLConnector = new MySQLConnector(config.get("Host"), config.get("Port"), config.get("Database"), config.get("Username"), config.get("Password"), true);
-        mySQLConnector.openConnection();
+        mySQLHandler = new MySQLHandler(config.get("Host"), config.get("Port"), config.get("Database"), config.get("Username"), config.get("Password"),true);
+        mySQLHandler.openConnection();
     }
 
     public void onDisable() {
 
     }
-
 }
