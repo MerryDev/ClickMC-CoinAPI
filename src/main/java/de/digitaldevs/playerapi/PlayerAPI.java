5f4a224fa66bc18a5df068756eacf923cd72f6c9
waddlespam <max@mlehrke.de>
2022-04-24 19:50:15 +0200

    Added MySQL Connector

--- src/main/java/de/digitaldevs/playerapi/PlayerAPI.java
+++ src/main/java/de/digitaldevs/playerapi/PlayerAPI.java
@@ -1,11 +1,19 @@
 package de.digitaldevs.playerapi;
 
+import de.digitaldevs.database.mysql.MySQLConnector;
+import de.digitaldevs.playerapi.utils.MySQLConfig;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PlayerAPI extends JavaPlugin {
 
+    private MySQLConfig config;
+
+    private MySQLConnector mySQLConnector;
     @Override
     public void onEnable() {
+        this.config = new MySQLConfig();
+        this.config.initDefault();
+        mySQLConnector = new MySQLConnector(config.get("Host"), config.get("Port"), config.get("Database"), config.get("Username"), config.get("Password"),true);
     }
 
     public void onDisable() {
