eaaa9d992d859358bab353facfd68d3db88e9568
waddlespam <max@mlehrke.de>
2022-04-24 19:28:25 +0200

    Initial commit

---
+++ src/main/java/de/digitaldevs/coins/command/CoinsCommand.java
@@ -0,0 +1,13 @@
+package de.digitaldevs.coins.command;
+
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class CoinsCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        return false;
+    }
+}
