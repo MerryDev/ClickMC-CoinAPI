221c56b89563331156b3e87ff753d0d7694878a2
MerryDev <svgr1808@outlook.de>
2022-04-24 20:10:20 +0200

    Opened connection to database

--- src/main/java/de/digitaldevs/playerapi/PlayerAPI.java
+++ src/main/java/de/digitaldevs/playerapi/PlayerAPI.java
@@ -2,21 +2,24 @@ package de.digitaldevs.playerapi;
 
 import de.digitaldevs.database.mysql.MySQLConnector;
 import de.digitaldevs.playerapi.utils.MySQLConfig;
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PlayerAPI extends JavaPlugin {
 
-    private MySQLConfig config;
+    @Getter
+    public MySQLConnector mySQLConnector;
 
-    private MySQLConnector mySQLConnector;
     @Override
     public void onEnable() {
-        this.config = new MySQLConfig();
-        this.config.initDefault();
-        mySQLConnector = new MySQLConnector(config.get("Host"), config.get("Port"), config.get("Database"), config.get("Username"), config.get("Password"),true);
+        MySQLConfig config = new MySQLConfig();
+        config.initDefault();
+        mySQLConnector = new MySQLConnector(config.get("Host"), config.get("Port"), config.get("Database"), config.get("Username"), config.get("Password"), true);
+        mySQLConnector.openConnection();
     }
 
     public void onDisable() {
 
     }
+
 }
